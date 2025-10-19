9a19260cf73c7a9ce23c778b68d7b66fdb75fbad
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-10-19 11:04:16 +0100

    Add cmake helpers

---
+++ cmake/write_desktop.cmake
@@ -0,0 +1,9 @@
+file(WRITE "${OUTPUT}" "[Desktop Entry]
+Type=Application
+Name=tr-driver
+Comment=GUI controller for LCD driver
+Exec=/usr/bin/gui_controller
+Icon=tr-driver
+Terminal=false
+Categories=Utility;
+")
